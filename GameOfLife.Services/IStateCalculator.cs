e5794c6e0a22e5fa1f8bc9cff2ef17bb253d032f
Stelmashenko-A <stelma_28@bk.ru>
2017-01-28 21:38:21 +0300

    #13 Refactoring StateCalculator

--- GameOfLife.Services/IStateCalculator.cs
+++ GameOfLife.Services/IStateCalculator.cs
@@ -9,27 +9,12 @@
     {
         public int NextState(int currentState, int neighborsNumber)
         {
-            if (currentState == 0 && neighborsNumber == 3)
-            {
-                return 1;
-            }
-
-            if (currentState == 1 && (neighborsNumber == 2 || neighborsNumber == 3))
-            {
-                return 1;
-            }
-
-            if (currentState == 1 && (neighborsNumber < 2 || neighborsNumber > 3))
+            if (neighborsNumber < 2 || neighborsNumber > 3)
             {
                 return 0;
             }
 
-            if (currentState == 0 && neighborsNumber != 3)
-            {
-                return 0;
-            }
-
-            throw new System.NotImplementedException();
+            return neighborsNumber == 3 ? 1 : currentState;
         }
     }
 }
