1c01c01513365d82f5904f1514041963c58e09dd
Stelmashenko-A <stelma_28@bk.ru>
2017-01-28 21:41:53 +0300

    #14 Move StateCalculator to separate file

--- GameOfLife.Services/IStateCalculator.cs
+++ GameOfLife.Services/IStateCalculator.cs
@@ -4,17 +4,4 @@
     {
         int NextState(int currentState, int neighborsNumber);
     }
-
-    public class StateCalculator : IStateCalculator
-    {
-        public int NextState(int currentState, int neighborsNumber)
-        {
-            if (neighborsNumber < 2 || neighborsNumber > 3)
-            {
-                return 0;
-            }
-
-            return neighborsNumber == 3 ? 1 : currentState;
-        }
-    }
 }
---
+++ GameOfLife.Services/StateCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace GameOfLife.Services
+{
+    public class StateCalculator : IStateCalculator
+    {
+        public int NextState(int currentState, int neighborsNumber)
+        {
+            if (neighborsNumber < 2 || neighborsNumber > 3)
+            {
+                return 0;
+            }
+
+            return neighborsNumber == 3 ? 1 : currentState;
+        }
+    }
+}
