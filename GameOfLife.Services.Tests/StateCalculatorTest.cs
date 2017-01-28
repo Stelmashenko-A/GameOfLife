d5d623a52e90be763084c42349c15161cb044145
Stelmashenko-A <stelma_28@bk.ru>
2017-01-28 21:23:26 +0300

    #11 [Test failed] Test for cell not born

--- GameOfLife.Services.Tests/StateCalculatorTest.cs
+++ GameOfLife.Services.Tests/StateCalculatorTest.cs
@@ -39,9 +39,26 @@ namespace GameOfLife.Services.Tests
         {
             IStateCalculator stateCalculator = new StateCalculator();
             const int currentState = IsAlive;
-            int[] neighborsNumbers = {0, 1, 4, 5, 6, 7, 8 };
+            int[] neighborsNumbers = { 0, 1, 4, 5, 6, 7, 8 };
             int[] expectedResult = { IsDead, IsDead, IsDead, IsDead, IsDead, IsDead, IsDead };
             int[] actualResult = new int[7];
+
+            for (var i = 0; i < neighborsNumbers.Length; i++)
+            {
+                actualResult[i] = stateCalculator.NextState(currentState, neighborsNumbers[i]);
+            }
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void CellNotBorn()
+        {
+            IStateCalculator stateCalculator = new StateCalculator();
+            const int currentState = IsDead;
+            int[] neighborsNumbers = { 0, 1, 2, 4, 5, 6, 7, 8 };
+            int[] expectedResult = { IsDead, IsDead, IsDead, IsDead, IsDead, IsDead, IsDead, IsDead };
+            int[] actualResult = new int[8];
 
             for (var i = 0; i < neighborsNumbers.Length; i++)
             {
