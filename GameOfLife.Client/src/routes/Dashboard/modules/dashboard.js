9fff115fd0fc3b248245f1720eff07dcc6faaffb
Artur Dzemianovich <Artur_Dzemianovich@epam.com>
2017-03-07 22:57:51 +0300

    update code to feat eslint rules.

--- GameOfLife.Client/src/routes/Dashboard/modules/dashboard.js
+++ GameOfLife.Client/src/routes/Dashboard/modules/dashboard.js
@@ -4,17 +4,17 @@ import _ from 'lodash'
 // ------------------------------------
 // Constants
 // ------------------------------------
-export const STEPS_CHANGE = 'STEPS_CHANGE',
-  PARTS_CHANGE = 'PARTS_CHANGE',
-  SIZE_CHANGE = 'WIDTH_CHANGE',
-  FIELD_INPUT_CHANGE = 'FIELD_INPUT_CHANGE',
-  SET_LOADING = 'SET_LOADING',
-  SET_LOADED = 'SET_LOADED',
-  FIELD_CHANGE = 'FIELD_CHANGE',
-  CURRENT_STEP_CHANGE = 'CURRENT_STEP_CHANGE',
-  STEPS_PATH_CHANGE = 'STEPS_PATH_CHANGE',
-  TASK_ID_CHANGE = 'TASK_ID_CHANGE',
-  PARTS_LOADED_CHANGE = 'PARTS_LOADED_CHANGE';
+export const STEPS_CHANGE = 'STEPS_CHANGE'
+export const PARTS_CHANGE = 'PARTS_CHANGE'
+export const SIZE_CHANGE = 'WIDTH_CHANGE'
+export const FIELD_INPUT_CHANGE = 'FIELD_INPUT_CHANGE'
+export const SET_LOADING = 'SET_LOADING'
+export const SET_LOADED = 'SET_LOADED'
+export const FIELD_CHANGE = 'FIELD_CHANGE'
+export const CURRENT_STEP_CHANGE = 'CURRENT_STEP_CHANGE'
+export const STEPS_PATH_CHANGE = 'STEPS_PATH_CHANGE'
+export const TASK_ID_CHANGE = 'TASK_ID_CHANGE'
+export const PARTS_LOADED_CHANGE = 'PARTS_LOADED_CHANGE'
 
 // ------------------------------------
 // Actions
@@ -69,31 +69,34 @@ export const setLoadedHandler = (loaded) => {
 }
 
 export const onStepsChangeHandler = (e) => {
+  var value = _.isNil(e.target.value) ? 0 : _.parseInt(e.target.value)
   return {
     type: STEPS_CHANGE,
-    payload: e.target.value
+    payload: value
   }
 }
 
 export const onPartsChangeHandler = (e) => {
+  var value = _.isNil(e.target.value) ? 0 : _.parseInt(e.target.value)
   return {
     type: PARTS_CHANGE,
-    payload: e.target.value
+    payload: value
   }
 }
 
 export const onSizeChangeHandler = (e) => {
-  var payload = e.target.value ? e.target.value : 0;
+  var value = _.isNil(e.target.value) ? 0 : _.parseInt(e.target.value)
+
   return {
     type: SIZE_CHANGE,
-    payload: payload
+    payload: value
   }
 }
 
 export const onFieldChangeHandler = (e) => {
   return {
     type: FIELD_INPUT_CHANGE,
-    payload: !((" " + e.target.className + " ").replace(/[\n\t]/g, " ").indexOf("active") > -1),
+    payload: !(('' + e.target.className + ' ').replace(/[\n\t]/g, ' ').indexOf('active') > -1),
     x: e.target.getAttribute('x'),
     y: e.target.getAttribute('y')
   }
@@ -103,210 +106,217 @@ export const onFieldChangeHandler = (e) => {
 // Functions
 // ------------------------------------
 export const nextButtonHandler = (e) => {
-  e.preventDefault();
+  e.preventDefault()
   return (dispatch, getState) => {
-    var state = getState().dashboard;
+    var state = getState().dashboard
 
-    if (state.currentStep == state.steps)
-      return;
+    if (state.currentStep === state.steps) {
+      return
+    }
 
-    dispatch(setCurrentStepHandler(state.currentStep + 1));
+    dispatch(setCurrentStepHandler(state.currentStep + 1))
 
-    state = getState().dashboard;
+    state = getState().dashboard
 
-    var matrix = generateMatrix(state.fieldSize, state.fieldSize, state.stepsPath, state.steps, state.currentStep);
+    var matrix = generateMatrix(state.fieldSize, state.fieldSize, state.stepsPath, state.steps, state.currentStep)
 
-    dispatch(setFieldHandler(matrix));
+    dispatch(setFieldHandler(matrix))
   }
-};
+}
 
 export const nextButtonMaxHandler = (e) => {
-  e.preventDefault();
+  e.preventDefault()
   return (dispatch, getState) => {
-    var state = getState().dashboard;
+    var state = getState().dashboard
 
-    if (state.currentStep == state.steps)
-      return;
+    if (state.currentStep === state.steps) {
+      return
+    }
 
-    dispatch(setLoadingHandler(true));
+    dispatch(setLoadingHandler(true))
 
     for (var i = 0; i < state.steps - state.currentStep; i++) {
       setTimeout(() => {
-        dispatch(setCurrentStepHandler(state.currentStep + 1));
+        dispatch(setCurrentStepHandler(state.currentStep + 1))
 
-        state = getState().dashboard;
+        state = getState().dashboard
 
-        var matrix = generateMatrix(state.fieldSize, state.fieldSize, state.stepsPath, state.steps, state.currentStep);
+        var matrix = generateMatrix(state.fieldSize, state.fieldSize, state.stepsPath, state.steps, state.currentStep)
 
-        dispatch(setFieldHandler(matrix));
+        dispatch(setFieldHandler(matrix))
 
-        if (state.currentStep == state.steps)
-          dispatch(setLoadingHandler(false));
-      }, 500 * i);
+        if (state.currentStep === state.steps) {
+          dispatch(setLoadingHandler(false))
+        }
+      }, 500 * i)
     }
   }
-};
+}
 
 export const prevButtonHandler = (e) => {
-  e.preventDefault();
+  e.preventDefault()
   return (dispatch, getState) => {
-    var state = getState().dashboard;
+    var state = getState().dashboard
 
-    if (state.currentStep === 0)
-      return;
+    if (state.currentStep === 0) {
+      return
+    }
 
-    dispatch(setCurrentStepHandler(state.currentStep - 1));
+    dispatch(setCurrentStepHandler(state.currentStep - 1))
 
-    state = getState().dashboard;
-    var matrix = generateMatrix(state.fieldSize, state.fieldSize, state.stepsPath, state.steps, state.currentStep);
+    state = getState().dashboard
+    var matrix = generateMatrix(state.fieldSize, state.fieldSize, state.stepsPath, state.steps, state.currentStep)
 
-    dispatch(setFieldHandler(matrix));
+    dispatch(setFieldHandler(matrix))
   }
-
-};
+}
 
 export const prevButtonMaxHandler = (e) => {
-  e.preventDefault();
+  e.preventDefault()
   return (dispatch, getState) => {
-    var state = getState().dashboard;
+    var state = getState().dashboard
 
-    if (state.currentStep == 0)
-      return;
+    if (state.currentStep === 0) {
+      return
+    }
 
-    dispatch(setLoadingHandler(true));
+    dispatch(setLoadingHandler(true))
 
     for (var i = 0; i < state.currentStep; i++) {
       setTimeout(() => {
-        dispatch(setCurrentStepHandler(state.currentStep - 1));
+        dispatch(setCurrentStepHandler(state.currentStep - 1))
 
-        state = getState().dashboard;
+        state = getState().dashboard
 
-        var matrix = generateMatrix(state.fieldSize, state.fieldSize, state.stepsPath, state.steps, state.currentStep);
+        var matrix = generateMatrix(state.fieldSize, state.fieldSize, state.stepsPath, state.steps, state.currentStep)
 
-        dispatch(setFieldHandler(matrix));
+        dispatch(setFieldHandler(matrix))
 
-        if (state.currentStep == 0)
-          dispatch(setLoadingHandler(false));
-
-      }, 500 * i);
+        if (state.currentStep === 0) {
+          dispatch(setLoadingHandler(false))
+        }
+      }, 500 * i)
     }
-  }
-
-};
-export const calculateHandler = (e) => {
-  e.preventDefault();
-  return (dispatch, getState) => {
-    dispatch(setLoadingHandler(true));
-	var data = getState().dashboard;
-
-	var path = _.clone(data.stepsPath);
-	path.push(matrixToString(data.field))
-
-	dispatch(setStepsPathHandler(path));
-
-	return axios({
-        method: 'Post',
-        url: '/values/process',
-        data: {
-          Id: null,
-          Field: matrixToString(data.field),
-          Steps: data.steps,
-          Parts: data.parts,
-        }
-      })
-      .then(function(response) {
-        if (response.data != null) {
-          dispatch(setLoadingHandler(false));
-          dispatch(setTaskIdHandler(response.data));
-
-          data = getState().dashboard;
-
-          getStepsPath(data.taskId, data.parts - data.partsLoaded - 1, data.partsLoaded, dispatch, getState);
-        }
-
-		dispatch(setLoadingHandler(false));
-      }).catch(function(error) {
-        dispatch(setLoadingHandler(false));
-      });
   }
 }
 
-function getStepsPath(taskId, part, partsLoaded, dispatch, getState) {
-  dispatch(setLoadingHandler(true));
+export const calculateHandler = (e) => {
+  e.preventDefault()
+  return (dispatch, getState) => {
+    dispatch(setLoadingHandler(true))
+    var data = getState().dashboard
+
+    var path = _.clone(data.stepsPath)
+    path.push(matrixToString(data.field))
+
+    dispatch(setStepsPathHandler(path))
+
+    return axios({
+      method: 'Post',
+      url: '/values/process',
+      data: {
+        Id: null,
+        Field: matrixToString(data.field),
+        Steps: data.steps,
+        Parts: data.parts
+      }
+    })
+      .then(function (response) {
+        if (response.data != null) {
+          dispatch(setLoadingHandler(false))
+          dispatch(setTaskIdHandler(response.data))
+
+          data = getState().dashboard
+
+          getStepsPath(data.taskId, data.parts - data.partsLoaded - 1, data.partsLoaded, dispatch, getState)
+        }
+
+        dispatch(setLoadingHandler(false))
+      }).catch(function (error) {
+        console.log(error)
+        dispatch(setLoadingHandler(false))
+      })
+  }
+}
+
+function getStepsPath (taskId, part, partsLoaded, dispatch, getState) {
+  dispatch(setLoadingHandler(true))
 
   return axios({
-      method: 'Get',
-      url: '/values/get/' + taskId + '/' + part
-    })
-    .then(function(response) {
-		var data = getState().dashboard;
+    method: 'Get',
+    url: '/values/get/' + taskId + '/' + part
+  })
+    .then(function (response) {
+      var data = getState().dashboard
 
-		if (response.data != "\"null\"") {
-			var preParse = JSON.parse(response.data);
-			var result = JSON.parse(preParse);
+      if (response.data !== '"null"') {
+        var preParse = JSON.parse(response.data)
+        var result = JSON.parse(preParse)
 
-			dispatch(setLoadingHandler(false));
+        dispatch(setLoadingHandler(false))
 
-			var paths = _.clone(data.stepsPath);
+        var paths = _.clone(data.stepsPath)
 
-			for (let path of result) {
-				paths.push(path);
-			}
+        for (let path of result) {
+          paths.push(path)
+        }
 
-			dispatch(setStepsPathHandler(paths));
+        dispatch(setStepsPathHandler(paths))
 
-			dispatch(setPartsLoadedHandler(partsLoaded + 1));
-		}
-
-		data = getState().dashboard;
-
-		if (data.parts != data.partsLoaded) {
-		  setTimeout(function() {
-			getStepsPath(data.taskId, data.parts - data.partsLoaded, data.partsLoaded, dispatch, getState);
-		  }, 1000);
-		} else {
-		  dispatch(setLoadedHandler(true));
-		}
-    }).catch(function(error) {
-      dispatch(setLoadingHandler(false));
-    });
-}
-
-
-function generateMatrix(x, y, stepsPath, steps, currentStep) {
-  var currentStepPath = null,
-    stepIndex = 0;
-  var matrix = new Array(x);
-  if (stepsPath !== null && stepsPath.length !== 0)
-    currentStepPath = stepsPath[currentStep];
-
-  for (var i = 0; i < x; i++) {
-    matrix[i] = new Array(y);
-    for (var j = 0; j < y; j++) {
-      if (currentStepPath !== null) {
-        matrix[i][j] = currentStepPath[stepIndex];
-
-      } else {
-        matrix[i][j] = 0;
+        dispatch(setPartsLoadedHandler(partsLoaded + 1))
       }
 
-      stepIndex++;
+      data = getState().dashboard
+
+      if (data.parts !== data.partsLoaded) {
+        setTimeout(function () {
+          getStepsPath(data.taskId, data.parts - data.partsLoaded, data.partsLoaded, dispatch, getState)
+        }, 1000)
+      } else {
+        dispatch(setLoadedHandler(true))
+      }
+    }).catch(function (error) {
+      if (error) {
+        console.log(error)
+      }
+      dispatch(setLoadingHandler(false))
+    })
+}
+
+function generateMatrix (x, y, stepsPath, steps, currentStep) {
+  var currentStepPath = null
+  var stepIndex = 0
+  var matrix = new Array(x)
+  if (stepsPath !== null && stepsPath.length !== 0) {
+    currentStepPath = stepsPath[currentStep]
+  }
+
+  for (var i = 0; i < x; i++) {
+    matrix[i] = new Array(y)
+    for (var j = 0; j < y; j++) {
+      if (currentStepPath !== null) {
+        matrix[i][j] = currentStepPath[stepIndex]
+      } else {
+        matrix[i][j] = 0
+      }
+
+      stepIndex++
     }
   }
 
-  return matrix;
+  return matrix
 }
 
-function matrixToString(matrix) {
-  var result = '';
+function matrixToString (matrix) {
+  var result = ''
 
   for (let row of matrix) {
     for (let col of row) {
-      result += col;
+      result += col
     }
   }
 
-  return result;
+  return result
 }
 
 export const actions = {
@@ -356,20 +366,19 @@ const ACTION_HANDLERS = {
     return Object.assign({}, state, {
       field: generateMatrix(action.payload, action.payload, state.stepsPath, state.steps, state.currentStep),
       fieldSize: action.payload
-    });
+    })
   },
   [FIELD_INPUT_CHANGE]: (state, action) => {
-    var field = _.map(state.field, _.clone);
-    field[action.x][action.y] = action.payload ? 1 : 0;
+    var field = _.map(state.field, _.clone)
+    field[action.x][action.y] = action.payload ? 1 : 0
 
     return Object.assign({}, state, {
       field: field
-    });
+    })
   }
 }
-
-function getIitialState() {
-  const fieldSize = 5;
+function getIitialState () {
+  const fieldSize = 5
 
   return {
     currentStep: 0,
@@ -383,12 +392,12 @@ function getIitialState() {
     partsLoaded: 0,
     taskId: null,
     host: ''
-  };
+  }
 }
 
-const initialState = getIitialState();
+const initialState = getIitialState()
 
-export default function dahsboardReducer(state = initialState, action) {
+export default function dahsboardReducer (state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
   return handler ? handler(state, action) : state
 }
